a997e10cad1bac349a68df65518054583af2357c
Hyeonki Hong <hhk7734@gmail.com>
2023-08-18 21:34:57 +0900

    feat: add ap-northeast-2.k0s.default

---
+++ ap-northeast-2/k0s/default/src/wiki.ts
@@ -0,0 +1,101 @@
+import * as kubernetes from "@pulumi/kubernetes";
+import * as networking from "@crds/istio/networking";
+
+const deployment = new kubernetes.apps.v1.Deployment("wiki", {
+	metadata: {
+		name: "wiki",
+		labels: {
+			"loliot.net/stack": "wiki",
+		},
+	},
+	spec: {
+		revisionHistoryLimit: 3,
+		selector: {
+			matchLabels: {
+				"app.kubernetes.io/name": "wiki",
+				app: "wiki",
+			},
+		},
+		template: {
+			metadata: {
+				labels: {
+					"app.kubernetes.io/name": "wiki",
+					app: "wiki",
+				},
+			},
+			spec: {
+				containers: [
+					{
+						name: "wiki",
+						image: "hhk7734/wiki-loliot-net:1.0",
+						imagePullPolicy: "Always",
+						ports: [
+							{
+								name: "http",
+								containerPort: 80,
+								protocol: "TCP",
+							},
+						],
+						resources: {
+							limits: {
+								memory: "64Mi",
+							},
+						},
+					},
+				],
+			},
+		},
+	},
+});
+
+const service = new kubernetes.core.v1.Service("wiki", {
+	metadata: {
+		name: "wiki",
+		labels: {
+			"loliot.net/stack": "wiki",
+		},
+	},
+	spec: {
+		ports: [
+			{
+				name: "http",
+				port: 8080,
+				protocol: "TCP",
+				targetPort: 80,
+			},
+		],
+		selector: deployment.spec.selector.matchLabels,
+		type: "ClusterIP",
+	},
+});
+
+const virtualService = new networking.v1beta1.VirtualService("wiki", {
+	metadata: {
+		name: "wiki",
+	},
+	spec: {
+		hosts: ["wiki.loliot.net"],
+		gateways: ["istio-system/default-gateway"],
+		http: [
+			{
+				match: [
+					{
+						uri: {
+							prefix: "/",
+						},
+					},
+				],
+				route: [
+					{
+						destination: {
+							host: "wiki",
+							port: {
+								number: 8080,
+							},
+						},
+					},
+				],
+			},
+		],
+	},
+});
