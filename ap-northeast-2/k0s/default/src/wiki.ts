b94ce5fb4927741383a6194184e6aa23c23ee024
Hyeonki Hong <hhk7734@gmail.com>
2024-01-24 00:06:31 +0900

    feat: bump wiki to 7f898481

--- ap-northeast-2/k0s/default/src/wiki.ts
+++ ap-northeast-2/k0s/default/src/wiki.ts
@@ -35,7 +35,7 @@ const deployment = new kubernetes.apps.v1.Deployment('wiki', {
 				containers: [
 					{
 						name: 'wiki',
-						image: 'ghcr.io/hhk7734/wiki:57e41948',
+						image: 'ghcr.io/hhk7734/wiki:7f898481',
 						imagePullPolicy: 'Always',
 						ports: [
 							{
