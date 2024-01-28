4818b021a42f4f672f02a2ef410feefb7366cbe5
Hyeonki Hong <hhk7734@gmail.com>
2024-01-28 14:56:52 +0900

    feat(k0s/auth): remove

--- ap-northeast-2/k0s/auth/src/cert-manager/index.ts
+++
@@ -1,2 +0,0 @@
-export * as certManager from './cert-manager';
-export * as clusterIssuer from './cluster-issuer';
--- ap-northeast-2/k0s/auth/src/variable.ts
+++
@@ -1,5 +0,0 @@
-import * as pulumi from '@pulumi/pulumi';
-
-export const stackName = pulumi.getStack();
-
-const config = new pulumi.Config();
